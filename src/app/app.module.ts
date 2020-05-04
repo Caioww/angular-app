84cb9c98b50b097f44c57a650ad853bf5b7b92d9
Caioww <sousinhacaio@gmail.com>
2020-05-04 13:18:34 -0300

    primeiro componente em angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponente } from './meu-primeiro-componente/meu-primeiro-componente';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponente
   ],
   imports: [
     BrowserModule
