87825d734007426aa2f096d647d51aeeb7333f28
Caioww <sousinhacaio@gmail.com>
2021-01-20 22:46:33 -0300

    criando modulo de formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MeuFormModule } from './meu-form/meu-form.module';
 import { CursosModule } from './cursos/cursos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,16 +6,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponente } from './meu-primeiro-componente/meu-primeiro-componente';
 import { MeuPrimeiroComponente2Component } from './meu-primeiro-componente2/meu-primeiro-componente2.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponente,
-    MeuPrimeiroComponente2Component
+    MeuPrimeiroComponente2Component,
+    DataBindingComponent,
+
   ],
   imports: [
     BrowserModule,
-    CursosModule
+    CursosModule,
+    FormsModule,
+    MeuFormModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = "http://loiane.com";
+  cursoAngular: boolean = true;
+  urlImagem = 'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg';
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
